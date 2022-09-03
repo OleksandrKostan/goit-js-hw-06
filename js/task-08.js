d0e9344d7ccefabcc20ce2f1ecaef8ab3e635b47
Oleksandr Kostan <kostan.law@gmail.com>
2022-09-03 15:30:45 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,32 @@
+// Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  //   const formData = new FormData(event.currentTarget);
+  //   formData.forEach((name, value) => {
+  //     console.log(name, value);
+  //   });
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  const formData = {
+    email,
+    password,
+  };
+  if (!email || !password) {
+    window.alert("Друже, всі поля повинні бути заповнені:)");
+  }
+  console.log(formData);
+  if (email && password) {
+    event.currentTarget.reset();
+  }
+}
